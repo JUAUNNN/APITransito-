5666d9cec4a06c84f798f20adadbaa579ba6d91c
JUAUNNN <joaovitor061jv@gmail.com>
2025-05-15 23:01:03 -0300

    feat: Refatorando interface ProprietarioRepository do pacote model para repository.

--- src/main/java/com/br/transito/controller/ProprietarioController.java
+++ src/main/java/com/br/transito/controller/ProprietarioController.java
@@ -1,7 +1,7 @@
 package com.br.transito.controller;
 
 import com.br.transito.domain.model.Proprietario;
-import com.br.transito.domain.model.ProprietarioRepository;
+import com.br.transito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/br/transito/domain/repository/ProprietarioRepository.java
+++ src/main/java/com/br/transito/domain/repository/ProprietarioRepository.java
@@ -1,5 +1,6 @@
-package com.br.transito.domain.model;
+package com.br.transito.domain.repository;
 
+import com.br.transito.domain.model.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
