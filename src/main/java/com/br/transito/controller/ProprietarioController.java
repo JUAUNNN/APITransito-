fe8efdc19529fb9ed5ad39a110b3b0d61c4883bc
JUAUNNN <joaovitor061jv@gmail.com>
2025-05-21 22:47:50 -0300

    feat: Adicionando endpoint de Get, Post e Put.

--- src/main/java/com/br/transito/controller/ProprietarioController.java
+++ src/main/java/com/br/transito/controller/ProprietarioController.java
@@ -3,19 +3,46 @@ package com.br.transito.controller;
 import com.br.transito.domain.model.Proprietario;
 import com.br.transito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
     private final ProprietarioRepository proprietarioRepository;
 
-    @GetMapping("/proprietarios ")
+    @GetMapping
     public List<Proprietario> listar(){
       return proprietarioRepository.findAll();
         //return  proprietarioRepository.findByNome("João Paulo Vieira");--Teste de costumização.
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar (@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario cadastrar (@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
+    }
+
+    @PutMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> atualizar (@PathVariable Long proprietarioId, @RequestBody Proprietario proprietario) {
+        if (!proprietarioRepository.existsById(proprietarioId)){
+            return ResponseEntity.notFound().build();
+        }
+
+        proprietario.setId(proprietarioId);
+        Proprietario proprietarioAtualizado = proprietarioRepository.save(proprietario);
+
+        return ResponseEntity.ok(proprietarioAtualizado);
     }
 }
