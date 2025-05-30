65af73bf915358284fcc91a3ac28b1b80a6bbfce
JUAUNNN <joaovitor061jv@gmail.com>
2025-05-29 22:17:45 -0300

    feat:Adicionando  regra de negócio para restringir e-mails duplicados.

--- src/main/java/com/br/transito/domain/service/CadastroProprietarioService.java
+++ src/main/java/com/br/transito/domain/service/CadastroProprietarioService.java
@@ -1,10 +1,13 @@
 package com.br.transito.domain.service;
 
+import com.br.transito.domain.exception.NegocioException;
 import com.br.transito.domain.model.Proprietario;
 import com.br.transito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @AllArgsConstructor
 @Service
@@ -14,6 +17,13 @@ public class CadastroProprietarioService {
 
     @Transactional
     public Proprietario salvar(Proprietario proprietario) {
+        boolean emailEmUso =  proprietarioRepository.findByEmail(proprietario.getEmail())
+                .filter(p -> !p.equals(proprietario))
+                .isPresent();
+
+        if (emailEmUso){
+            throw new NegocioException("Já existe um proprietário cadastrado com este e-mail!");
+        }
         return proprietarioRepository.save(proprietario);
     }
 
