c79551936fd70372297c5fdd46cb8adc90d49754
JUAUNNN <joaovitor061jv@gmail.com>
2025-06-05 22:36:43 -0300

    refactor: Customizando as mensagens de validação, utilizando messages.properties

--- src/main/java/com/br/transito/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/br/transito/exceptionHandler/ApiExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.br.transito.exceptionHandler;
 
 import com.br.transito.domain.exception.NegocioException;
+import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,9 +16,14 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+@AllArgsConstructor
 @RestControllerAdvice
 public class ApiExceptionHandler  extends ResponseEntityExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid
@@ -24,6 +33,13 @@ public class ApiExceptionHandler  extends ResponseEntityExceptionHandler {
         ProblemDetail problemDetail = ProblemDetail.forStatus(status);
         problemDetail.setTitle("Um ou mais campos estão inválidos");
         problemDetail.setType(URI.create("https://apiTransito.com/erros/campos-ivalidos"));
+
+       Map<String, String> fields = ex.getBindingResult().getAllErrors()
+                .stream()
+                .collect(Collectors.toMap(objectError -> ((FieldError) objectError).getField(),
+                        objectError -> messageSource.getMessage(objectError, LocaleContextHolder.getLocale())));
+
+       problemDetail.setProperty("fields", fields);
 
         return handleExceptionInternal(ex, problemDetail, headers, status, request);
     }
